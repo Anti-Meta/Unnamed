14d11d0c200fea4ea6c47b9919c317dca7324745
Darius Wattimena <Darius Wattimena>
2017-03-18 22:40:38 +0100

    First Commit

---
+++ desktop/src/nl/antimeta/unnamed/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package nl.antimeta.unnamed.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import nl.antimeta.unnamed.Unnamed;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Unnamed(), config);
+	}
+}
